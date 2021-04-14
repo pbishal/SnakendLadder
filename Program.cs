6e54ec888109e8b40517e0ef5b9d7a4654318e00
pbishal <pbishal60@gmail.com>
2021-04-14 13:00:08 +0530

    Dice value generated

--- Program.cs
+++ Program.cs
@@ -4,13 +4,24 @@ namespace SnakeLadder
 {
     class Program
     {
+        static void rollDie()
+        {
+            Random rand = new Random();
+            int diceValue = rand.Next(1, 6);
+            Console.WriteLine("the dice value is:" + diceValue);
+
+        }
         static void Main(string[] args)
         {
             
             int INITIAL_POSITION = 0;
             int NUMBER_OF_PLAYERS = 1;
             Console.WriteLine("Welcome to snake and ladder game");
+
+            rollDie();
             Console.ReadLine();
+
+
         }
     }
 }

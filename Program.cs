72ab7041072a2d118f5014cf71302e7890e98c6d
pbishal <pbishal60@gmail.com>
2021-04-14 12:18:03 +0530

    Initial position added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snake and ladder problem");
+            
+            int INITIAL_POSITION = 0;
+            int NUMBER_OF_PLAYERS = 1;
+            Console.WriteLine("Welcome to snake and ladder game");
+            Console.ReadLine();
         }
     }
 }

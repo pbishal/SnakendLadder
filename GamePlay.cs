254414cb1d88c12fb0fe7b6758aa6d6c75254a4b
pbishal <pbishal60@gmail.com>
2021-04-16 12:41:00 +0530

    Check Option Added

--- GamePlay.cs
+++ GamePlay.cs
@@ -7,11 +7,29 @@ namespace SnakeLadder
     class GamePlay
     {
         const int START_POSITION = 0;
+        const int NO_PLAY = 0;
+        const int LADDER = 1;
+        const int SNAKE = 2;
         public void Play()
         {
             Random random = new Random();
+            int option = random.Next(0, 3);
             int dieValue = random.Next(1, 7);
-            Console.WriteLine(" Player die roll position = " + dieValue);
+            int player = 0;
+            switch (option)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play, Pass the chance");
+                    break;
+                case LADDER:
+                    player += dieValue;
+                    Console.WriteLine("You got Ladder and the  player position is: " + player);
+                    break;
+                case SNAKE:
+                    player -= dieValue;
+                    Console.WriteLine("You gotSnake and the player position is: " + player);
+                    break;
+            }
         }
     }
 }

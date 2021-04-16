542af4d4c4857a26f1b44e3cdff2d01421150e84
pbishal <pbishal60@gmail.com>
2021-04-16 12:21:55 +0530

    Modified

--- Program.cs
+++ Program.cs
@@ -4,12 +4,30 @@ namespace SnakeLadder
 {
     class Program
     {
+        //int noPlay = 0;
         static void rollDie()
         {
             Random rand = new Random();
-            int diceValue = rand.Next(1, 6);
+            int diceValue = rand.Next(1, 7);
             Console.WriteLine("the dice value is:" + diceValue);
-
+        }
+        static void checkOption()
+        {
+            Random rand = new Random();
+            int play = rand.Next(1, 4);
+            Console.WriteLine("the dice value is:" + play);
+            if(play==1)
+            {
+                Console.WriteLine("No Play:the player stays in the same position");
+            }
+            else if(play==2)
+            {
+                Console.WriteLine("ladder");
+            }
+            else
+            {
+                Console.WriteLine("snake");
+            }
         }
         static void Main(string[] args)
         {
@@ -17,8 +35,10 @@ namespace SnakeLadder
             int INITIAL_POSITION = 0;
             int NUMBER_OF_PLAYERS = 1;
             Console.WriteLine("Welcome to snake and ladder game");
-
+            Console.WriteLine("initial position is:" + INITIAL_POSITION);
             rollDie();
+
+            checkOption();
             Console.ReadLine();
 
 

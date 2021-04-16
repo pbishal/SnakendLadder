e60d07d4b148e6cb7d77bc44325a8f4cd67c8086
pbishal <pbishal60@gmail.com>
2021-04-16 12:34:08 +0530

    Dice Roll generated

--- GamePlay.cs
+++ GamePlay.cs
@@ -6,5 +6,12 @@ namespace SnakeLadder
 {
     class GamePlay
     {
+        const int START_POSITION = 0;
+        public void Play()
+        {
+            Random random = new Random();
+            int dieValue = random.Next(1, 7);
+            Console.WriteLine(" Player die roll position = " + dieValue);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,45 +3,12 @@
 namespace SnakeLadder
 {
     class Program
-    {
-        //int noPlay = 0;
-        static void rollDie()
-        {
-            Random rand = new Random();
-            int diceValue = rand.Next(1, 7);
-            Console.WriteLine("the dice value is:" + diceValue);
-        }
-        static void checkOption()
-        {
-            Random rand = new Random();
-            int play = rand.Next(1, 4);
-            Console.WriteLine("the dice value is:" + play);
-            if(play==1)
-            {
-                Console.WriteLine("No Play:the player stays in the same position");
-            }
-            else if(play==2)
-            {
-                Console.WriteLine("ladder");
-            }
-            else
-            {
-                Console.WriteLine("snake");
-            }
-        }
+    {       
         static void Main(string[] args)
         {
-            
-            int INITIAL_POSITION = 0;
-            int NUMBER_OF_PLAYERS = 1;
-            Console.WriteLine("Welcome to snake and ladder game");
-            Console.WriteLine("initial position is:" + INITIAL_POSITION);
-            rollDie();
-
-            checkOption();
-            Console.ReadLine();
-
-
+            Console.WriteLine("Welcome to Snake and Ladder game");
+            GamePlay gamePlay = new GamePlay();
+            gamePlay.Play();
         }
     }
 }
